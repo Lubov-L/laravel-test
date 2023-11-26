02e9101e70975de3c89ad5c1eb5de87017faa392
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-26 22:45:13 +0300

    рефакторинг кода

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Worker extends Model
+class Profile extends Model
 {
     protected $guarded = false;
 }
