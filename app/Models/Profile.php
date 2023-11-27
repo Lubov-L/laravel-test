701d0388eab41e91826a3beb18ad5ee06334974e
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-11-27 18:15:29 +0300

    построение отношений один к одному

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profile extends Model
 {
     protected $guarded = false;
+
+    public function worker(): BelongsTo
+    {
+        return $this->belongsTo(Worker::class, 'worker_id', 'id');
+    }
 }
--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Worker extends Model
 {
     protected $guarded = false;
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class, 'worker_id', 'id');
+    }
 }
